a97c9d438ec4cfd86f612f304fd5a1b7f2743d3d
“amonsalv” <anamonsalve96@gmail.com>
2023-05-12 08:42:31 -0500

    componentes menu y home y manejo de apps

--- hotelesquimbaya/src/app/app-routing.module.ts
+++ hotelesquimbaya/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; 
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = []; //vamos a enrutar desde componentes
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
