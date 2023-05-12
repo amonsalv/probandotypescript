a6f6d96f0b7ede3f3ee79190ff8f89b2a4d630d7
“amonsalv” <anamonsalve96@gmail.com>
2023-05-12 07:50:11 -0500

    ejemplos de ts y angular

---
+++ probandotypescript/src/Comida.ts
@@ -0,0 +1,4 @@
+export interface Comida{
+    nombre:string,
+    precio?:number
+}
---
+++ probandotypescript/src/aprendiendo.ts
@@ -0,0 +1,13 @@
+import { Comida } from "./Comida"
+
+let nombre:string="Ana María"
+let edad:number=26
+let yaDesayuno:boolean=false
+
+let notas:number[]=[3,4.5,2.8]//para los arreglos ponemos los [] al lado de nuestro tipo
+let equiposFavoritos:string[]=[] //
+
+let comidaFavorita:Comida={
+    nombre:"Tacos",
+    precio:25000
+}
